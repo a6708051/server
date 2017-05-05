77d20656e8f1c5a33563549cc6210fd2fb6e70ef
kary_abc <530219765@qq.com>
2017-05-05 17:55:21 +0800

    test

--- src/app/test/TestServer.php
+++ src/app/test/TestServer.php
@@ -22,13 +22,13 @@ class TestServer extends server\support\Socket
     public static function onConnect($socket_server, $fd, $from_id)
     {
         echo "Client:Connect.\n";
-        header("location:www.baidu.com");
-        exit;
         $socket_server->send($fd, 'welcome to here');
     }
 
     public static function onReceive($socket_server, $fd, $from_id, $data)
     {
+        header("location:www.baidu.com");
+        exit;
         $socket_server->send($fd, 'Server: '.$data);
     }
 
